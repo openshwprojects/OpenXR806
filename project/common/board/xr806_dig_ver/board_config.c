7fd929f31d22f9f0198c3fdd7673d745614e7621
divadiow <divadiow@hotmail.com>
2026-04-21 17:41:09 +0100

    Switch XR806 app flash read mode to DIO

--- project/common/board/xr806_dig_ver/board_config.c
+++ project/common/board/xr806_dig_ver/board_config.c
@@ -196,7 +196,7 @@ static const FlashBoardCfg g_flash_cfg[] = {
 	{
 #if 1
 		.type = FLASH_DRV_FLASHC,
-		.mode = FLASH_READ_QUAD_O_MODE,
+		.mode = FLASH_READ_DUAL_IO_MODE,
 		.clk = (64 * 1000 * 1000),
 #else /* for spi */
 		.type = FLASH_DRV_SPI,
